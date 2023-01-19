23d333092fda4d6b1f8878207046fcb17d05f362
Emrah Sozlu <emrahsozlu@hotmail.com>
2023-01-20 00:53:31 +0300

    code updated

--- Main.java
+++ Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double a,b,c,alan,cevre,u;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("1. kenar uzunluğunu giriniz : ");
+        a = scanner.nextDouble();
+        System.out.print("2. kenar uzunluğunu giriniz : ");
+        b = scanner.nextDouble();
+        System.out.print("3. kenar uzunluğunu giriniz : ");
+        c = scanner.nextDouble();
+
+        u = (a+b+c)/2;
+        cevre = 2*u;
+        alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+
+        System.out.println("Üçgenin Çevresi : "+ cevre);
+        System.out.println("Üçgenin Alanı : "+ alan);
+
+    }
 }
